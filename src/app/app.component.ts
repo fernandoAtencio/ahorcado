550ac0a96ccdb3440a799cdf8c21126ccdc4805c
Fernando Atencio <atencio.fernando@gmail.com>
2021-01-02 12:08:21 -0300

    Juego del ahorcado realizado con Angular palabras random

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,80 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ahorcado';
+  palabrasArr = ["AGUACATE", "SALVAJE", "LOCOMOTORA", "METRALLETA", "TORO", "CAMA","ASADO", "TELEVISOR", "ORNITORRINCO"]
+  numRamdom = Math.floor(Math.random() * ((this.palabrasArr.length+1) - 0) + 0);
+  palabra = this.palabrasArr[Math.floor(Math.random() * ((this.palabrasArr.length+1) - 0) + 0)];
+  palabraOculta = '';
+  intentos = 0;
+  gano = false;
+  perdio = false;
+  letras = [
+    'A', 
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'Ñ',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ];
+  constructor() {
+    this.palabraOculta = '_ '.repeat(this.palabra.length);
+  }
+  
+  comprobar(letra: string) {
+    this.existeLetra(letra);
+    const palabraOcultaArr = this.palabraOculta.split(' ');
+    for (let i = 0; i < this.palabra.length; i++) {
+      if (this.palabra[i] === letra) {
+        palabraOcultaArr[i] = letra;
+      }
+    }
+    this.palabraOculta = palabraOcultaArr.join(' ');
+    this.verificaGane();
+  }
+
+  verificaGane() {
+    let palabraArr = this.palabraOculta.split(' ');
+    const palabraEvaluar = palabraArr.join('');
+
+    if (palabraEvaluar === this.palabra) {
+      this.gano = true;
+      console.log('Ganaste!!!!!!');
+    }
+    if (this.intentos >= 9) {
+      this.perdio = true;
+      console.log('Alpiste Perdisssste!!!!!!!!!');
+    }
+  }
+
+  existeLetra(letra: string) {
+    if (this.palabra.indexOf(letra) >= 0) {
+      //console.log('letra existe ' + letra);
+    } else {
+      //console.log('letra no existe ' + letra);
+      this.intentos++;
+    }
+  }
 }
